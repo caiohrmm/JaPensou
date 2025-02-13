fcf53de114e228b7ebe8284f8a714f40b8845d4b
Caio Henrique <cahenromar@gmail.com>
2025-02-13 15:13:34 -0300

    Adicionando variaveis de ambiente

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,21 @@
-// Fazendo a conexao com o banco pelo sequelize
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("japensou", "root", "caio123", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
+
 try {
   sequelize.authenticate();
   console.log("Conectado ao banco de dados!");
 } catch (error) {
-  console.log(`Não foi possível conectar ao banco de dados : ${error}`);
+  console.log(`Não foi possível conectar ao banco de dados: ${error}`);
 }
 
 module.exports = sequelize;
